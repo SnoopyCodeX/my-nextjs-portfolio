7bf9adf79b7eeb5a2b56e6d9f0c1e110109ced6e
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-26 11:39:50 +0800

    Updated cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -42,7 +42,7 @@ export default function Intro() {
                     Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1" />
                 </Link>
 
-                <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full ouline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer dark:bg-white/10" href="/CV.pdf" download>
+                <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full ouline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer dark:bg-white/10" href="/johnroylapidacalimlim-cv.pdf" download>
                     Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
                 </a>
                 
