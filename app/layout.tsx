dab0ef44577327a041c5f24d2305a65edda477b2
SnoopyCodeX <extremeclasherph@gmail.com>
2023-12-11 06:21:39 +0800

    Fixed theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 }) {
 
   return (
-    <html lang="en" className='!scroll-smooth'>
+    <html lang="en" className='!scroll-smooth dark'>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-[hsla(240deg, 20%, 20%, 1)] dark:text-gray-50 dark:text-opacity-90`}>
         {/*         
         <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
