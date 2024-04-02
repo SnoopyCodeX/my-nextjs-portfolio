21e1154f54261f241a7a408a0368dd0da09fd8ea
SnoopyCodeX <extremeclasherph@gmail.com>
2024-04-03 03:07:01 +0800

    Fixed spacing in date for experience section

--- components/project.tsx
+++ components/project.tsx
@@ -24,13 +24,13 @@ export default function Project({
     const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
     return (
-        <motion.div ref={ref} style={{scale: scaleProgress, opacity: opacityProgress}} className="group mb-3 sm:mb-8 last:mb-0">
+        <motion.div ref={ref} style={{scale: scaleProgress, opacity: opacityProgress}} className="mb-3 group sm:mb-8 last:mb-0">
             <section className='bg-gray-100 max-w-[42rem] border backdrop-blur-[4px] border-black/5 rounded-lg overflow-hidden sm:pr-8 sm:h-[20rem] relative sm:group:even:pl-8 hover:bg-gray-200 hover:scale-105 hover:shadow transition dark:bg-white/10 dark:hover:bg-white/20 dark:text-white'>
 
                 <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
                     <h3 className="text-2xl font-semibold">{title}</h3>
                     <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
-                    <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
+                    <ul className='flex flex-wrap gap-2 mt-4 sm:mt-auto'>
                         {tags.map((tag, index) => (
                             <li className="bg-black/[0.7] py-1 px-3 text-[0.7rem] uppercase tracking-wider dark:text-white/70 text-white rounded-full" key={index}>
                                 {tag}
@@ -39,7 +39,7 @@ export default function Project({
                     </ul>
                 </div>
 
-                <Image className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:-translate-y-3 group-even:group-hover:rotate-2 group-hover:scale-[1.04] transition hidden sm:block" src={imageUrl} alt={title} quality={95} />
+                <Image className="absolute top-8 -right-40 w-[28.25rem] h-full rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:-translate-y-3 group-even:group-hover:rotate-2 group-hover:scale-[1.04] transition hidden sm:block" src={imageUrl} alt={title} height={921} quality={95} />
             </section>
         </motion.div>
     );
